e5c08c26e5bd5fa2e2294a193b6c3dd1bb59e708
Marco Santana <santannacostamarco@gmail.com>
2018-10-28 22:22:37 -0200

    Serial monitor

--- serial_collector.py
+++ serial_collector.py
@@ -4,15 +4,6 @@
 import serial
 import mysql.connector
 
-def readSerial(serialDevice):
-	try:
-		serialData = arduinoBoard.readline()
-		print serialData
-		return readSerial(serialDevice)
-	except:
-		print "Failed to read Serial data"
-		return
-
 dbConn = mysql.connector.connect(
 	host = "127.0.0.1", # localhost mysql server (can be a remote server)
 	user = "root", 
@@ -22,7 +13,35 @@ dbConn = mysql.connector.connect(
 
 dbCursor = dbConn.cursor()
 
-serialPort = "/dev/ttyACM0" # Serial port used by Arduino board, it chages.
+serialPort = "/dev/ttyACM0" # Serial port used by Arduino board, it may change.
+
+def clearData(data):
+	data = data.replace("\\r\\n", '')
+	return data.split(" ")
+	pass
+
+def getDataDate(data):
+	return data[1] + " " + data[2]
+	pass
+
+def getDataDirection(data):
+	return data[0]
+	pass
+
+def recordData(direction, date):
+	print direction + " " + date
+	pass
+
+def readSerial(serialDevice):
+	try:
+		serialData = arduinoBoard.readline()
+		serialData = clearData(serialData)
+		recordData(getDataDirection(serialData), getDataDate(serialData))
+		return readSerial(serialDevice)
+	except:
+		print "Failed to read Serial data"
+		return
+	pass
 
 try:
 	print "Trying...",serialPort 
